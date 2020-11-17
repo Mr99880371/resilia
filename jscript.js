4fda80efa706959fe660151feb2114f78b58fb5b
Mr99880371 <mariane_araujo@id.uff.br>
2020-11-17 16:24:38 -0300

    Jogo interativo atualizado

--- jscript.js
+++ jscript.js
@@ -1,26 +1,33 @@
 //Jogo Interativo
-
 var h1 = document.querySelector('h1');
-var cabeça = document.querySelectorAll('#cabeçalho');
+var cabeca = document.querySelectorAll('#cabeçalho');
 var resultado = document.getElementById('resultado');
 var bot1 = document.querySelector('#afirma');
 var bot2 = document.querySelector('#nega');
 
+function Recarrega() {
+    addEventListener('click', (bot1) => {
+        window.location.reload();
+    })
+}
+
 function afirma() {
     var texto = document.querySelector("#cabeçalho p").textContent = ('~ Você resolveu sair, mas está protegido?? ~')
-    h1.parentNode.removeChild(h1)
     bot1.innerHTML = "Sim, levei uma máscara e álcool pra me proteger! :)"
     bot2.innerHTML = "Não levei nada para me proteger! :("
     bot1.onclick = afirma1
     bot2.onclick = nega1
+    h1.parentNode.removeChild(h1);
+
 }
 
 function nega() {
-    var texto = document.querySelector("#cabeçalho p").textContent = ('~ Você resolveu ficar em casa, mas o problema da comida ainda não foi solucionado. ~' + '~ Você abre o seu armário e dá de cara com uma lata de sardinha fechada, o que você vai fazer? ~')
+    var texto = document.querySelector("#cabeçalho p").textContent = (`~ Você resolveu ficar em casa, mas o problema da comida ainda não foi solucionado. ~~ Você abre o seu armário e dá de cara com uma lata de sardinha fechada, o que você vai fazer? ~`)
     bot1.innerHTML = "Abro a lata para verificar se ainda está bom o alimento"
     bot2.innerHTML = "Jogo a lata fora, pois percebo que já faz um tempo e pode estar fora da validade"
     bot1.onclick = afirma2
     bot2.onclick = nega2
+    h1.parentNode.removeChild(h1);
 }
 
 function afirma1() {
@@ -33,10 +40,10 @@ function afirma1() {
 }
 
 function nega1() {
-    var texto = document.querySelector("#cabeçalho p").textContent = ('~ Você encontrou uma pessoa contaminada e foi contaminado!! ~~~ GAME OVER!!')
-    h1.innerHTML = "Você perdeu!!!!"
-    bot1.parentNode.removeChild('#afirma')
-    bot2.parentNode.removeChild('#nega')
+    var texto = document.querySelector("#cabeçalho p").textContent = ('~ Você encontrou uma pessoa contaminada e foi contaminado!! ~~' + '~ GAME OVER!!');
+    bot1.innerHTML = "Você perdeu!!!!"
+    bot1.onclick = Recarrega
+    bot2.parentNode.removeChild(bot2);
 }
 
 function afirma2() {
@@ -45,6 +52,7 @@ function afirma2() {
     bot2.innerHTML = "Saio de casa, pois tenho família para alimentar e preciso urgente comprar comida."
     bot1.onclick = afirma4
     bot2.onclick = afirma
+        //NÃO FUMCIONOU:
         //next.innerHTML = `<button id ="afirma" onclick="afirma4()"></button>`
         //next2.innerHTML = `<button id="nega" onclick="nega4()"></button>`
 }
@@ -55,6 +63,7 @@ function nega2() {
     bot2.innerHTML = "Saio de casa, pois tenho família para alimentar e preciso urgente comprar comida."
     bot1.onclick = afirma4
     bot2.onclick = afirma
+        //NÃO FUNCIONOU:
         //next.innerHTML = `<button id="afirma" onclick="afirma4()"></button>`
         //next2.innerHTML = `<button id="nega" onclick="nega4()"></button>`
 }
@@ -65,10 +74,12 @@ function afirma3() {
     bot2.innerHTML = "Ignoro o estado da atendente e continuo a pagar minhas compras mesmo assim."
     bot1.onclick = afirma5
     bot2.onclick = nega5
+        //NÃO FUNCIONOU:
         //next.innerHTML = `<button id="afirma" onclick="afirma5()"></button>`
         //next2.innerHTML = `<button id="nega" onclick="nega5()"></button>`
 }
 
+//NÃO FUNCIONOU:
 // function nega3() {
 //     var texto = document.querySelector("#cabeçalho p").textContent = ('~ Você encontrou uma pessoa contaminada e foi contaminado!! ~~~ GAME OVER!')
 //     h1.innerHTML = "Você perdeu!!!!"
@@ -80,10 +91,12 @@ function afirma4() {
     bot2.innerHTML = "morro de fome"
     bot1.onclick = afirma
     bot2.onclick = nega6
+        //NÃO FUNCIONOU:
         //`<button id="afirma" onclick="afirma6()"></button>`
         //`<button id="nega" onclick="nega6()">morro de fome.</button>`
 }
 
+//NÃO FUNCIONOU:
 // function nega4() {
 //     var texto = document.querySelector("#cabeçalho p").textContent = ('~ Você resolveu sair, mas está protegido?? ~')
 //     bot1.innerHTML = "Sim, levei uma máscara e álcool pra me proteger!"
@@ -98,15 +111,19 @@ function afirma5() {
     bot2.innerHTML = "Me irrito e saio do mercado sem comprar nada."
     bot1.onclick = afirma7
     bot2.onclick = nega7
+        //NÃO FUNCIONOU:
         //`<button id="afirma" onclick="afirma7()"></button>`
         //`<button id="nega" onclick="nega7()"></button>`
 }
 
 function nega5() {
     var texto = document.querySelector("#cabeçalho p").textContent = ('~ Você foi contaminado e depois de alguns dias morreu!! ~')
-    h1.innerHTML = "Você perdeu!!!!"
+    bot1.innerHTML = "Você perdeu!!!!"
+    bot1.onclick = Recarrega
+    bot2.parentNode.removeChild(bot2)
 }
 
+//NÃO FUNCIONOU:
 // function afirma6() {
 //     var texto = document.querySelector("#cabeçalho p").textContent = ('~ Você resolveu sair, mas está protegido?? ~')
 //     var next = document.querySelector("#afirma")
@@ -117,7 +134,9 @@ function nega5() {
 
 function nega6() {
     var texto = document.querySelector("#cabeçalho p").textContent = ('~ Você morreu!! ~')
-    h1.innerHTML = "Você perdeu!!!!"
+    bot1.innerHTML = "Você perdeu!!!!"
+    bot1.onclick = Recarrega
+    bot2.parentNode.removeChild(bot2)
 }
 
 function afirma7() {
@@ -126,6 +145,7 @@ function afirma7() {
     bot2.innerHTML = "Saio desse supermercado e tento encontrar outro."
     bot1.onclick = afirma8
     bot2.onclick = nega7
+        //NÃO FUNCIONOU:
         //`<button id="afirma" onclick="afirma8()"></button>`
         //`<button id="nega" onclick="nega8()"></button>`
 }
@@ -136,16 +156,19 @@ function nega7() {
     bot2.innerHTML = "Volto para casa sem comprar nada."
     bot1.onclick = afirma9
     bot2.onclick = nega9
+        //NÃO FUNCIONOU:
         //`<button id="afirma" onclick="afirma9()"></button>`
         //`<button id="nega" onclick="nega9()"></button>`
 }
 
 function afirma8() {
     var texto = document.querySelector("#cabeçalho p").textContent = ('~ Depois de horas na fila finalmente consigo voltar pra casa sã e salvo e com minhas compras. ~')
-    h1.innerHTML = "Parabéns você venceu!!!"
-        //`<button id="afirma" onclick="afirma8()">.</button>`
+    bot1.innerHTML = "Parabéns você venceu!!!"
+    bot1.onclick = Recarrega
+    bot2.parentNode.removeChild(bot2);
 }
 
+//NÃO FUNCIONOU:
 // function nega8() {
 //     var texto = document.querySelector("#cabeçalho p").textContent = ('Você encontra outro supermercado em que os funcionários estão mais protegidos. Porém nesse os preços são mais caros. o que você vai fazer? ~')
 //     var next = document.querySelector("#afirma")
@@ -156,11 +179,16 @@ function afirma8() {
 
 function afirma9() {
     var texto = document.querySelector("#cabeçalho p").textContent = ('~ Consigo fazer minhas compras, apesar de gastar mais, mas volto pra casa sã e salvo. ~')
-    h1.innerHTML = "Parabéns você venceu!!!"
-        //`<button id="afirma" onclick="afirma8()">.</button>`
+    bot1.innerHTML = "Parabéns você venceu!!!"
+    bot1.onclick = Recarrega
+    bot2.parentNode.removeChild(bot2);
+    //NÃO FUNCIONOU:
+    //`<button id="afirma" onclick="afirma8()">.</button>`
 }
 
 function nega9() {
     var texto = document.querySelector("#cabeçalho p").textContent = ('~ Você voltou para casa, mas como não há nada para comer, você morreu!! ~')
-    h1.innerHTML = "Você perdeu!!!!"
+    bot1.innerHTML = "Você perdeu!!!!"
+    bot1.onclick = Recarrega
+    bot2.parentNode.removeChild(bot2);
 }
